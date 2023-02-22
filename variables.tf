b4f21fb08713849b2bbcb4c8010b7d9b9969cc54
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-02-22 18:29:11 +0400

    Terragrunt adopotion

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable "region" {}
-
 variable "stack" {
+  type = string
   description = "Stack name"
 }
 
 variable "nginx_ingress_namespace" {
+  type = string
   description = "Namespace where nginx ingress will be deployed to"
   default     = "nginx-ingress"
 }
@@ -15,6 +15,12 @@ variable "domain" {
 }
 
 variable "nginx_ingress_kind" {
-  description = "Kind of Nginx ingress controller. Can be Deployment/DaemonSet"
+  type = string
+  description = "Kind of Nginx ingress controller"
   default     = "Deployment"
+
+  validation {
+    condition = contains(["Deployment", "DaemonSet"], var.nginx_ingress_kind)
+    error_message = "Kind can be either Deployment or DaemonSet"
+  }
 }
