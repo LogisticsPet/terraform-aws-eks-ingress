0239f350b175b26505d3ef2543ec2a05874d4038
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-03-19 14:07:11 +0400

    Remove outputs after switch to external DNS
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lb_arn" {
-  description = "ARN of LB created by ingress controller"
-  value       = data.aws_lb.lb.arn
-}
-
-output "lb_dns_name" {
-  description = "DNS name of LB created by ingress controller"
-  value       = data.aws_lb.lb.dns_name
-}
