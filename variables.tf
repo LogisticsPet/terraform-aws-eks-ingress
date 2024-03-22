22aa098c052e6391ed55e1cd8723f602c6391c0d
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-22 21:18:07 +0100

    Refactor kubernetes authentication
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- variables.tf
+++ variables.tf
@@ -4,11 +4,6 @@ variable "stack" {
   default     = "dev"
 }
 
-variable "cluster_name" {
-  type        = string
-  description = "Name of EKS cluster"
-}
-
 variable "namespace" {
   type        = string
   description = "Namespace where nginx ingress and AWS LB Controller will be deployed to"
@@ -28,4 +23,27 @@ variable "nginx_ingress_kind" {
 variable "oidc_provider_arn" {
   type        = string
   description = "OIDC provider arn"
+}
+
+
+variable "cluster_endpoint" {
+  sensitive   = true
+  type        = string
+  description = "Endpoint of the cluster."
+}
+
+variable "cluster_ca" {
+  sensitive   = true
+  type        = string
+  description = "CA certificate of the cluster."
+}
+
+variable "k8s_exec_args" {
+  type        = list(string)
+  description = "Args for Kubernetes provider exec plugin. Example command ['eks', 'get-token', '--cluster-name', '{clusterName}}']"
+}
+
+variable "k8s_exec_command" {
+  type        = string
+  description = "Command name for Kubernetes provider exec plugin. Example - 'aws"
 }
